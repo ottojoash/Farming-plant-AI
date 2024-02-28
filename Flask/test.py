56049d139683b6d660471e337dbd94bbc84be52d
ottojoash <louisjoshbricks@gmail.com>
2024-02-28 16:19:49 +0300

    working

--- Flask/test.py
+++ Flask/test.py
@@ -1,84 +1,93 @@
 
 from dotenv import find_dotenv, load_dotenv
 import os
+
+from langchain_openai import OpenAIEmbeddings
 load_dotenv(find_dotenv())
 os.getenv("OPENAI_API_KEY")
-# import
-from langchain.text_splitter import CharacterTextSplitter
-from langchain_community.document_loaders import TextLoader
+
+
+
+# # import
+# from langchain.text_splitter import CharacterTextSplitter
+# from langchain_community.document_loaders import TextLoader
 # from langchain_community.embeddings.sentence_transformer import (
 #     SentenceTransformerEmbeddings,
 # )
-from langchain_community.embeddings import HuggingFaceEmbeddings
-
-from langchain_community.vectorstores import Chroma
-from langchain_community.document_loaders import DirectoryLoader
-from langchain_openai import OpenAIEmbeddings
-
-from chromadb.utils import embedding_functions
+# from langchain_community.vectorstores import Chroma
 
 
-# load the document and split it into chunks
-# loader = TextLoader("../../modules/state_of_the_union.txt")
-loader = DirectoryLoader('Flask\\chromadocs', glob="**/*.md", show_progress=True,use_multithreading=True)
+# # load the document and split it into chunks
+# loader = TextLoader("C:\\Users\\JOASH\\Downloads\\Compressed\\Farming-plant-AI-master\\Flask\\chromadocs\\PPA-46.txt")
+# documents = loader.load()
 
-
-documents = loader.load()
-print("******loading docs****")
-
-# split it into chunks
-text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-docs = text_splitter.split_documents(documents)
+# # split it into chunks
+# text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+# docs = text_splitter.split_documents(documents)
 
 # # create the open-source embedding function
-
-print("******at embeddings function****")
 embedding_function = OpenAIEmbeddings(model="text-embedding-3-large")
 
+# # embedding_function = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+# db= Chroma.from_documents(docs, embedding_function, persist_directory="./chroma_db")
 
-
-# load it into Chroma
-# db = Chroma.from_documents(docs, embedding_function)
-db = Chroma(persist_directory="chroma_db", embedding_function=embedding_function)
-
-# retriever = db.as_retriever()
-retriever = db.as_retriever(search_type="mmr")
-
-print("******retriving docs****")
-
-# query it
-query = "what is a plant disease and give me examples"
+# # query it
+# query = "what is a plant"
 # docs = db.similarity_search(query)
-docs = retriever.get_relevant_documents(query)
-# print results
-print(docs[0].page_content)
 
+# # print results
+# print(docs[0].page_content)
 
-#*******************************
-
-# from langchain.agents import AgentType, Tool, initialize_agent
-# from langchain_community.utilities import GoogleSerperAPIWrapper
-# from langchain_community.llms import OpenAI  # Update the import statement
-
-# llm = OpenAI(temperature=0)
-# search = GoogleSerperAPIWrapper()
-# tools = [
-#     Tool(
-#         name="Intermediate Answer",
-#         func=search.run,
-#         description="useful for when you need to ask with search",
-#     )
-# ]
-
-# self_ask_with_search = initialize_agent(
-#     tools, llm, agent=AgentType.SELF_ASK_WITH_SEARCH, verbose=True
-# )
-# self_ask_with_search.run(
-#     "What is the hometown of the reigning men's U.S. Open champion?"
-# )
+from langchain_core.messages import AIMessage
+from langchain_core.messages import HumanMessage
 
 from langchain.vectorstores import Chroma
 from langchain.embeddings.openai import OpenAIEmbeddings
-persist_directory = 'docs/chroma/'
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.chains.combine_documents import create_stuff_documents_chain
+
+llm = ChatOpenAI(model="gpt-3.5-turbo-1106", temperature=0.2)
+
 embedding = OpenAIEmbeddings()
-vectordb = Chroma(persist_directory=persist_directory, embedding_function=embedding)
+vectordb = Chroma(persist_directory="./chroma_db", embedding_function=embedding)
+print(vectordb._collection.count())
+
+db = Chroma(persist_directory="./chroma_db", embedding_function=embedding_function)
+
+# retriever = db.as_retriever(search_type="mmr")
+query="what is a plant disease"
+retriever = db.as_retriever(k=1)
+
+docs = retriever.invoke(query)
+
+
+
+chat = ChatOpenAI(model="gpt-3.5-turbo-1106")
+
+question_answering_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "Answer the user's questions based on the below context:\n\n{context}",
+        ),
+        MessagesPlaceholder(variable_name="messages"),
+    ]
+)
+
+document_chain = create_stuff_documents_chain(chat, question_answering_prompt)
+
+# print(document_chain)
+from langchain.memory import ChatMessageHistory
+
+demo_ephemeral_chat_history = ChatMessageHistory()
+
+demo_ephemeral_chat_history.add_user_message(query)
+
+res = document_chain.invoke(
+    {
+        "messages": demo_ephemeral_chat_history.messages,
+        "context": docs,
+    }
+)
+print(res)
